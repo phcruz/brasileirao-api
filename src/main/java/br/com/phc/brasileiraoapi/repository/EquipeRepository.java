b3f99673ba11da42e77bcb75bad02f4cf39b91ba
da Cruz <pedc@gft.com>
2020-10-11 19:10:23 -0300

    Implementação de controller, service e repository de equipe

--- src/main/java/br/com/phc/brasileiraoapi/repository/EquipeRepository.java
+++ src/main/java/br/com/phc/brasileiraoapi/repository/EquipeRepository.java
@@ -1,5 +1,7 @@
 package br.com.phc.brasileiraoapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import br.com.phc.brasileiraoapi.entity.Equipe;
 @Repository
 public interface EquipeRepository extends JpaRepository<Equipe, Long>{
 
+	public Optional<Equipe> findByNomeEquipe(String nomeEquipe);
+	
+	public boolean existsByNomeEquipe(String nomeEquipe);
 }
