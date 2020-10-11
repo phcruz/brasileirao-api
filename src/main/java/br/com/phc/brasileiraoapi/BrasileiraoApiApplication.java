a804307d3e1bc4b5207709ab9b028d79cc4a9432
da Cruz <pedc@gft.com>
2020-10-11 15:47:14 -0300

    Subindo a versao inicial do projeto

---
+++ src/main/java/br/com/phc/brasileiraoapi/BrasileiraoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.phc.brasileiraoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phc/brasileiraoapi/BrasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phc.brasileiraoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
